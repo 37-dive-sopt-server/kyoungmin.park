aca45150ca3971df5d2c2fdfa89419981ceac130
kymin <park48664866@gmail.com>
2025-10-11 17:32:03 +0900

    feat: 1차 세미나 실습

---
+++ src/main/java/org/sopt/controller/MemberController.java
@@ -0,0 +1,26 @@
+package org.sopt.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.sopt.domain.Member;
+import org.sopt.service.MemberService;
+import org.sopt.service.MemberServiceImpl;
+
+public class MemberController {
+
+	private final MemberService memberService = new MemberServiceImpl();
+
+	public Long createMember(String name) {
+
+		return memberService.join(name);
+	}
+
+	public Optional<Member> findMemberById(Long id) {
+		return memberService.findOne(id);
+	}
+
+	public List<Member> getAllMembers() {
+		return memberService.findAllMembers();
+	}
+}
