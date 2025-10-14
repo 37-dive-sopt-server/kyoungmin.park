d7d6a040ba43a13b6fc42bec657c7bfa222a2514
kymin <park48664866@gmail.com>
2025-10-15 00:16:01 +0900

    feat: 직렬화 추가

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,8 +1,9 @@
 package org.sopt.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Member {
+public class Member implements Serializable {
 
 	private Long id;
 	private String name;
