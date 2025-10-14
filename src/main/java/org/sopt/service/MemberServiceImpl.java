36d039ed24c787590449f981b1f9418162734db0
kymin <park48664866@gmail.com>
2025-10-14 13:43:46 +0900

    feat: 회원 삭제, 이메일 중복 방지, 연령 제한 구현

--- src/main/java/org/sopt/service/MemberServiceImpl.java
+++ src/main/java/org/sopt/service/MemberServiceImpl.java
@@ -1,21 +1,38 @@
 package org.sopt.service;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Optional;
 
+import org.sopt.domain.Gender;
 import org.sopt.domain.Member;
-import org.sopt.repository.MemoryMemberRepository;
+import org.sopt.exception.EmailAlreadyExistException;
+import org.sopt.exception.InvalidAgeException;
+import org.sopt.exception.message.ExceptionMessage;
+import org.sopt.repository.MemberRepository;
+import org.sopt.repository.MemberRepositoryFacade;
+import org.sopt.service.util.IdGenerator;
 
 public class MemberServiceImpl implements MemberService {
 
-	private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-	private static long sequence = 1L;
+	private final MemberRepository memberRepository = new MemberRepositoryFacade();
 
-	public Long join(String name) {
+	public void initData() {
+		memberRepository.init();
+	}
 
-		Member member = new Member(sequence++, name);
-		memberRepository.save(member);
-		return member.getId();
+	public Long join(String name, String email, LocalDate birthday, Gender gender) {
+		if (calculateAge(birthday) < 20) {
+			throw new InvalidAgeException(ExceptionMessage.AGE_UNDER_20.getMessage());
+		}
+		if (memberRepository.existsByEmail(email)) {
+			throw new EmailAlreadyExistException(ExceptionMessage.ALREADY_EXIST_EMAIL.getMessage());
+		}
+
+		Member member = new Member(IdGenerator.generate(), name, email, birthday, gender);
+
+		return memberRepository.save(member).getId();
 	}
 
 	public Optional<Member> findOne(Long memberId) {
@@ -24,5 +41,13 @@ public class MemberServiceImpl implements MemberService {
 
 	public List<Member> findAllMembers() {
 		return memberRepository.findAll();
+	}
+
+	public void deleteMemberById(Long id) {
+		memberRepository.deleteById(id);
+	}
+
+	private int calculateAge(LocalDate birthday) {
+		return Period.between(birthday, LocalDate.now()).getYears();
 	}
 }
