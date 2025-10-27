bcbc7041347ebff369f10cf5425105498eb03854
kymin <park48664866@gmail.com>
2025-10-27 16:01:08 +0900

    refactor: 예외 핸들러 추상화 및 스프링 마이그레이션

--- src/main/java/org/sopt/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/handler/GlobalExceptionHandler.java
@@ -1,17 +1,35 @@
 package org.sopt.global.exception.handler;
 
+import org.sopt.global.api.code.ErrorCode;
 import org.sopt.global.api.response.ApiResponse;
 import org.sopt.global.exception.BaseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends BaseExceptionHandler {
 
 	@ExceptionHandler(BaseException.class)
 	protected ResponseEntity<ApiResponse<Void>> handleBaseException(BaseException e) {
-		return ResponseEntity.status(e.getErrorCode().getHttpStatus())
-				.body(ApiResponse.failure(e.getErrorCode()));
+		return buildErrorResponse(e.getErrorCode());
+	}
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleMethodArgumentTypeMismatchException(
+			MethodArgumentTypeMismatchException e) {
+		return buildErrorResponse(ErrorCode.INVALID_REQUEST_MESSAGE);
+	}
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+		return buildErrorResponse(ErrorCode.INVALID_ENDPOINT);
+	}
+
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+		return buildErrorResponse(ErrorCode.UNDEFINED_ERROR);
 	}
 }
