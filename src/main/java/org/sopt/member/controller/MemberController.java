d87bd8d552d6356ed12f5dbb4acb57c2155dc821
kymin <park48664866@gmail.com>
2025-10-29 11:17:03 +0900

    feat: 스웨거 코드 추가

--- src/main/java/org/sopt/member/controller/MemberController.java
+++ src/main/java/org/sopt/member/controller/MemberController.java
@@ -7,13 +7,21 @@ import org.sopt.member.dto.request.MemberCreateDto;
 import org.sopt.member.dto.response.MemberInfoDto;
 import org.springframework.http.ResponseEntity;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "회원 관리 기능", description = "회원 정보의 생성, 조회, 삭제 관련 API")
 public interface MemberController {
 
+	@Operation(summary = "회원 정보 생성", description = "이름, 이메일, 생년월일, 성별 등의 정보로 회원 정보를 생성합니다.")
 	ResponseEntity<ApiResponse<Void>> createMember(MemberCreateDto memberCreateDto);
 
+	@Operation(summary = "특정 회원 정보 조회", description = "식별자에 해당하는 회원 아이디를 통해 특정 회원의 정보를 조회합니다.")
 	ResponseEntity<ApiResponse<MemberInfoDto>> findMemberById(long id);
 
+	@Operation(summary = "전체 회원 정보 조회", description = "전체 회원에 대한 정보를 조회합니니다.")
 	ResponseEntity<ApiResponse<List<MemberInfoDto>>> getAllMembers();
 
+	@Operation(summary = "회원 정보 삭제", description = "식별자에 해당하는 회원 아이디를 통해 특정 회원의 정보를 삭제합니다.")
 	ResponseEntity<ApiResponse<Void>> deleteMemberById(long id);
 }
